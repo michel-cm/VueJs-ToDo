859f98028ed825a8dd8b43298d35a737fa37c454
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-23 23:14:16 -0300

    create form

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,48 @@
 <template>
   <div id="app">
-    <div>hello world</div>
+    <div class="container grid-xs py-2">
+      <img
+        class="img-responsive imgLogo"
+        src="@/assets/logo.png"
+        alt="logomarca"
+      />
+      <form @submit.prevent="addTodo(todo)">
+        <div class="input-group">
+          <input
+            type="text"
+            v-model="todo.description"
+            class="form-input"
+            placeholder="Novo todo"
+          />
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+      </form>
+      {{ todos }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {},
+  data() {
+    return {
+      todos: [],
+      todo: { checked: false },
+    };
+  },
+  methods: {
+    addTodo(todo) {
+      todo.id = Date.now();
+      this.todos.push(todo);
+    },
+  },
 };
 </script>
+
+<style scoped>
+.imgLogo {
+  max-width: 80px;
+  margin: 0 auto;
+}
+</style>
